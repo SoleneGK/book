282c8c18af4127cc76a04e6a9c5f78d9115516b5
Solène <solene.garda.krebs@gmail.com>
2020-05-23 19:39:21 +0200

    Series support added

--- src/Admin/BookAdmin.php
+++ src/Admin/BookAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Admin;
 
 use App\Entity\Genre;
+use App\Entity\Series;
 use App\Entity\Author;
 use App\Entity\Rating;
 use App\Entity\Language;
@@ -14,6 +15,7 @@ use Sonata\AdminBundle\Form\Type\ModelType;
 use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 final class BookAdmin extends AbstractAdmin
 {
@@ -27,24 +29,37 @@ final class BookAdmin extends AbstractAdmin
 			->add('title', TextType::class, [
 				'label' => 'Titre',
 			])
+			->add('series', ModelType::class, [
+				'class' => Series::class,
+				'required' => false,
+				'label' => 'Série',
+			])
+			->add('number_in_series', IntegerType::class, [
+				'label' => 'N° du tome',
+				'required' => false,
+			])
 			->add('authors', ModelType::class, [
 				'class' => Author::class,
 				'multiple' => true,
+				'label' => 'Auteurice(s)',
 			])
 			->add('genres', ModelType::class, [
 				'class' => Genre::class,
 				'property' => 'name',
 				'multiple' => true,
+				'label' => 'Genres',
 			])
 			->add('rating', EntityType::class, [
 				'class' => Rating::class,
 				'choice_label' => 'code',
-				'required' => false
+				'required' => false,
+				'label' => 'Note',
 			])
 			->add('languages', EntityType::class, [
 				'class' => Language::class,
 				'choice_label' => 'name',
 				'multiple' => true,
+				'label' => 'Langue(s)',
 			])
 		;
     }
---
+++ src/Migrations/Version20200523171848.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200523171848 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE series (id INT AUTO_INCREMENT NOT NULL, name LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE book ADD series_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A3315278319C FOREIGN KEY (series_id) REFERENCES series (id)');
+        $this->addSql('CREATE INDEX IDX_CBE5A3315278319C ON book (series_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE book DROP FOREIGN KEY FK_CBE5A3315278319C');
+        $this->addSql('DROP TABLE series');
+        $this->addSql('DROP INDEX IDX_CBE5A3315278319C ON book');
+        $this->addSql('ALTER TABLE book DROP series_id');
+    }
+}
